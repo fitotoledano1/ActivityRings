8749936c675c49a2a7905e17cf865ff04bf0c5ca
Fito Toledano <fito.toledano@gmail.com>
2024-08-30 01:32:45 +0200

    Initial Commit

---
+++ ActivityRings/ActivityRingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityRingsApp.swift
+//  ActivityRings
+//
+//  Created by Fito Toledano on 30/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ActivityRingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivityRings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ActivityRings
+//
+//  Created by Fito Toledano on 30/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
